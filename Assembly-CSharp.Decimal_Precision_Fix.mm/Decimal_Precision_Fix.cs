c701ed9202f56dfa7c6e5a16cfccde0ab4a4a32c
MassiveBattlebotsFan <teamlightning2018@outlook.com>
2023-10-25 11:37:46 -0500

    Add project files.

---
+++ Assembly-CSharp.Decimal_Precision_Fix.mm/Decimal_Precision_Fix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MonoMod;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+public static class patch_BotlabUtilities
+{
+    [MonoModReplace]
+    static patch_BotlabUtilities()
+    {
+        patch_BotlabUtilities.decimalDigits = 7;
+    }
+    private static int decimalDigits;
+}
